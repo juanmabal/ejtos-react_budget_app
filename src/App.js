7b83b5d01247b2524992e6b4d9f407ccd0ad5ee4
Juan Abal <juanete3244@hotmail.com>
2023-03-04 12:12:27 -0500

    tryng to push

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,8 +15,14 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}        
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
 
                         {/* Add Remaining component here under */}        
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
 
                         {/* Add ExpenseTotal component here under */}        
                        
